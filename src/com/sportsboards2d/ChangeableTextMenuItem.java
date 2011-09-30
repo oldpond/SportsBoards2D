b24973362d9305580715618577edccc4c2c70a6b
oldpond <oldpond@gmail.com>
2011-09-30 08:05:08 -0500

    Added GPL text.

--- src/com/sportsboards2d/ChangeableTextMenuItem.java
+++ src/com/sportsboards2d/ChangeableTextMenuItem.java
@@ -4,10 +4,21 @@ import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
 import org.anddev.andengine.entity.text.ChangeableText;
 import org.anddev.andengine.opengl.font.Font;
 
-
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with SportsBoards2D.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Copyright 2011 5807400 Manitoba Inc. All rights reserved.
+ * Copyright 2011 5807400 Manitoba Inc. 
  */
 public class ChangeableTextMenuItem extends ChangeableText implements IMenuItem{
 	
